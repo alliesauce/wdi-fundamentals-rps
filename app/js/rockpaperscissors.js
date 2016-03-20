c1b7eebe50c1423f7beb803365fa4b86864ff17b
Alli Cummings <alli.cummings@gmail.com>
2016-03-20 17:22:35 -0400

    added comment on getWinner function, not returning correct winner

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -41,6 +41,7 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
+    //my winners are not coming out correctly. Not sure why. If I manually add playerMove and computerMove to getWinner, the result is correct (eg getWinner('rock', 'paper') ==> player
     if (playerMove === 'rock' && computerMove === 'scissors') {
     	winner = "player";
     } else if (playerMove === 'rock' && computerMove === 'paper') {
@@ -89,4 +90,4 @@ function playToFive() {
     }
     return [playerWins, computerWins];
 }
-playToFive(5);
+
